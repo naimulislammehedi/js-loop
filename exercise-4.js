7ce6e38fb6c6f2edfc08c77ecebbfcbb90a34803
Mehedi <mehedi.interface@gmail.com>
2026-06-01 09:48:46 +0600

    4. Prints each fruit from: [Apple, Banana, Mango, Orange]

---
+++ exercise-4.js
@@ -0,0 +1,8 @@
+// 4. Prints each fruit from:
+// ["Apple", "Banana", "Mango", "Orange"]
+
+const fruits = ["Apple", "Banana", "Mango", "Orange"]; 
+
+for (const fruit of fruits) {
+    console.log(fruit); 
+}
